55c0b0438c331610809c6e6023a0c1420c41d1fa
YeonwooSung <neos960518@gmail.com>
2019-07-13 18:02:02 +0900

    update app.js

--- ble-test/App.js
+++ ble-test/App.js
@@ -1,19 +1,93 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  Alert,
+  Button,
+  ListView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+var noble = require('react-native-ble');
+
+export default class ReactNativeBle extends Component {
+  constructor() {
+    super();
+
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      scanText: 'Initializing...',
+      buttonText: "Turn ON",
+      bleDevices: [],
+      mainDataSource: ds
+    }
+  }
+
+  onButtonPress = () => {
+    Alert.alert('Button has been pressed!');
+  };
+
+  onDevicePressed = (device) => {
+    Alert.alert('Device pressed: ' + device);
+  };
+
+  componentWillMount() {
+    noble.on('stateChange', this._onStateChange.bind(this));
+    //noble.on('discover', this._onDiscover.bind(this));
+
+    this.setState({ scanText: "Waiting for poweredOn event..." });
+  }
+
+  _onStateChange(state) {
+    if (state === 'poweredOn') {
+      noble.startScanning();
+      this.setState({ scanText: "Scanning for devices.." });
+    } else {
+      noble.stopScanning();
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.instructions}>
+          {this.state.scanText}
+        </Text>
+        <View style={styles.devices}>
+          <ListView dataSource={this.state.mainDataSource} renderRow={(rowData) => <Text onPress={this.onDevicePressed.bind(this, rowData)} style={styles.deviceRow}>{rowData}</Text>} />
+        </View>
+        <Button onPress={this.onButtonPress} title={this.state.buttonText} color="#841584" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  devices: {
+    height: "25%",
+    width: "70%",
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    margin: 10
+  },
+  deviceRow: {
+    textAlign: 'center',
+    padding: 5
+  }
 });
