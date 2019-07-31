1a8705308049e59e71be686db4a45cace0205a09
YeonwooSung <neos960518@gmail.com>
2019-07-31 11:27:17 +0900

    add uuids for sending deviceName via BLE

--- ble-test/src/BLEUtil.js
+++ ble-test/src/BLEUtil.js
@@ -6,6 +6,8 @@ const WIFI_NAME_SERVICE_UUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
 const WIFI_PW_SERVICE_UUID = '4fafc202-1fb5-459e-8fcc-c5c9c331914b';
 const WIFI_NAME_CHARACTERISTIC_UUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
 const WIFI_PW_CHARACTERISTIC_UUID = 'bed5483e-36e1-4688-b7f5-ea07361b26a8';
+const DEVICE_NAME_SERVICE_UUID = "4fafc203-1fb5-459e-8fcc-c5c9c331914b";
+const DEVICE_NAME_CHARACTERISTIC_UUID = "bec5483e-36e1-4688-b7f5-ea07361b26a8";
 
 
 let sendData_wifi = (device_uuid, wifi, pw) => {
@@ -34,11 +36,11 @@ let sendData_deviceName = (device_uuid, deviceName) => {
 
     const deviceName_data = stringToBytes(deviceName);
 
-    BleManager.write(device_uuid, WIFI_NAME_SERVICE_UUID, WIFI_NAME_CHARACTERISTIC_UUID, deviceName_data, deviceName_data.length + 1)
+    BleManager.write(device_uuid, DEVICE_NAME_SERVICE_UUID, DEVICE_NAME_CHARACTERISTIC_UUID, deviceName_data, deviceName_data.length + 1)
         .then(() => {
             console.log('success');
         })
-        .catch((err) => { alert('error::write wifi') });
+        .catch((err) => { alert('error::write deviceName') });
 }
 
 let connectBLEDevice = (uuid, next, handleError) => {
@@ -54,4 +56,4 @@ let connectBLEDevice = (uuid, next, handleError) => {
 
 module.exports.connectBLEDevice = connectBLEDevice;
 module.exports.sendData_wifi = sendData_wifi;
-module.exports.sendData_deviceNum = sendData_deviceNum;
+module.exports.sendData_deviceName = sendData_deviceName;
