66fb968b66101381459714d0ae170f2320e76b25
YeonwooSung <neos960518@gmail.com>
2019-09-12 02:03:57 +0900

    Now the splash screen appears when launch the app

--- 9roomit/android/app/src/main/java/com/groomit/MainApplication.java
+++ 9roomit/android/app/src/main/java/com/groomit/MainApplication.java
@@ -3,6 +3,7 @@ package com.groomit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.amazonaws.RNAWSCognitoPackage;
 import com.horcrux.svg.SvgPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SplashScreenReactPackage(),
             new RNAWSCognitoPackage(),
             new SvgPackage(),
             new RNGestureHandlerPackage(),
