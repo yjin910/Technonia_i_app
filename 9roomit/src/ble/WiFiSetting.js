3a55d8f0e14841db29542c2ab6095469ded8cc68
YeonwooSung <neos960518@gmail.com>
2019-09-15 21:40:36 +0900

    Fixed keyboard avoiding error, and rebuild the app

--- 9roomit/src/ble/WiFiSetting.js
+++ 9roomit/src/ble/WiFiSetting.js
@@ -14,11 +14,30 @@ import {
     ScrollView,
     Image
 } from 'react-native';
+import Drawer from 'react-native-drawer'
+import uuidv1 from 'uuid/v1';
+import { StackActions, NavigationActions } from 'react-navigation';
 
+import DrawerButton from '../graph/components/DrawerButton'
+import Footer from '../Footer';
+
+
+let util = require('./BLEUtil');
 
 const { width, height } = Dimensions.get('window');
 
-let util = require('./BLEUtil');
+const LOGO_IMAGE = require('../../assets/logo.png');
+const BACK_IMAGE = require('../../assets/back.png');
+const MENU_IMAGE = require('../../assets/menu.png');
+
+const menu = [
+    { title: 'Main' },
+    { title: 'Profile' },
+    { title: 'Setting' },
+    { title: 'Log out' },
+    { title: 'Help' },
+    { title: 'Copyright' },
+]
 
 export default class WiFiSetting extends React.Component {
     constructor(props) {
@@ -31,6 +50,120 @@ export default class WiFiSetting extends React.Component {
         }
 
         this.sendWiFiSettings = this.sendWiFiSettings.bind(this);
+
+        this.goBack = this.goBack.bind(this);
+        this.goBack_Drawer = this.goBack_Drawer.bind(this);
+        this.logOut_async = this.logOut_async.bind(this);
+        this.navigateToMainScreen = this.navigateToMainScreen.bind(this);
+        this.navigateToHelpScreen = this.navigateToHelpScreen.bind(this);
+        this.navigateToCopyrightScreen = this.navigateToCopyrightScreen.bind(this);
+        this.navigateToProfileScreen = this.navigateToProfileScreen.bind(this);
+    }
+
+    static navigationOptions = {
+        header: null
+    };
+
+    openDrawer() {
+        this.drawer.open()
+    }
+
+    closeDrawer() {
+        this.drawer.close()
+    }
+
+    goBack_Drawer = () => {
+        this.closeDrawer();
+        this.goBack();
+    }
+
+    logOut_async = async () => {
+        //TODO disconnect device
+
+        await AsyncStorage.removeItem('9room@email');
+        await AsyncStorage.removeItem('9room@autoLogin');
+
+        const resetAction = StackActions.reset({
+            index: 0,
+            actions: [NavigationActions.navigate({ routeName: 'Login' })],
+        });
+
+        this.props.navigation.dispatch(resetAction);
+    }
+
+    navigateToMainScreen = async () => {
+        //TODO disconnect device
+
+        this.closeDrawer();
+        let email = await AsyncStorage.getItem('9room@email');
+        this.props.navigation.navigate('Main', { email: email });
+    }
+
+    navigateToHelpScreen = () => {
+        //TODO disconnect device
+
+        this.closeDrawer();
+        this.props.navigation.navigate('Help');
+    }
+
+    navigateToCopyrightScreen = () => {
+        //TODO disconnect device
+
+        this.closeDrawer();
+        this.props.navigation.navigate('Copyright');
+    }
+
+    navigateToProfileScreen = async () => {
+        //TODO disconnect device
+
+        this.closeDrawer();
+        let email = await AsyncStorage.getItem('9room@email');
+        this.props.navigation.navigate('Profile', { email: email });
+    }
+
+    goBack = () => {
+        this.props.navigation.goBack();
+    }
+
+    renderDrawer = () => {
+        const MENU_VIEW = menu.map((item, index) => {
+            let title = item.title;
+            let onPress;
+
+            switch (title) {
+                case 'Setting':
+                    onPress = this.goBack_Drawer;
+                    break;
+                case 'Log out':
+                    onPress = this.logOut_async;
+                    break;
+                case 'Main':
+                    onPress = this.navigateToMainScreen;
+                    break;
+                case 'Profile':
+                    onPress = this.navigateToProfileScreen;
+                    break;
+                case 'Help':
+                    onPress = this.navigateToHelpScreen;
+                    break;
+                case 'Copyright':
+                    onPress = this.navigateToCopyrightScreen;
+                    break;
+                default:
+                    console.log('Invalid title: ', title);
+                    onPress = null;
+            }
+
+            return (<DrawerButton onPress={onPress} title={title} key={uuidv1()} />);
+        });
+
+        return (
+            <SafeAreaView style={styles.drawerContainer}>
+                <View style={styles.drawerContainer}>
+                    {MENU_VIEW}
+                </View>
+            </SafeAreaView>
+        );
     }
 
     sendWiFiSettings = async () => {
@@ -52,47 +185,91 @@ export default class WiFiSetting extends React.Component {
             <KeyboardAvoidingView
                 keyboardVerticalOffset={Platform.select({ ios: 0, android: width / 3 })}
                 style={styles.keyboardAvoidingContainer}
-                behavior={"padding"}
+                behavior={Platform.OS === "ios" ? "padding" : null}
                 enabled>
-                <SafeAreaView style={styles.container}>
-                    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                        <ScrollView>
-                            <View style={styles.container}>
-                                <TextInput style={styles.inputBox}
-                                    placeholder="Wi-Fi"
-                                    placeholderTextColor="#1a3f95"
-                                    selectionColor="#fff"
-                                    keyboardType="email-address"
-                                    onChangeText={(wifi) => this.setState({ wifi: wifi })}
-                                    value={this.state.wifi}
-                                />
-                                <TextInput style={styles.inputBox}
-                                    placeholder="Password"
-                                    placeholderTextColor="#1a3f95"
-                                    onChangeText={(pw) => this.setState({ pw: pw })}
-                                    value={this.state.pw}
-                                />
-                                <TouchableOpacity style={styles.buttonBox} onPress={() => this.sendWiFiSettings()}>
-                                    <Text style={styles.buttonText}>OK</Text>
+                <SafeAreaView style={styles.root}>
+                    <Drawer
+                        ref={(ref) => this.drawer = ref}
+                        content={this.renderDrawer()}
+                        type='overlay'
+                        tapToClose={true}
+                        openDrawerOffset={0.6}
+                        styles={drawerStyles}
+                        side={'right'}
+                    >
+                        <View style={styles.headerContainer}>
+                            <View style={styles.menuButton}>
+                                <TouchableOpacity
+                                    onPress={() => this.goBack()}
+                                    style={{ tintColor: 'white', width: width / 9, height: width / 9, marginRight: width / 30, justifyContent: 'center' }}>
+                                    <Image style={{ tintColor: 'white', width: width / 9 - 10, height: width / 9 - 10 }} source={BACK_IMAGE} />
                                 </TouchableOpacity>
                             </View>
-                        </ScrollView>
-                    </TouchableWithoutFeedback>
+                            <Image style={{ width: width / 3, height: height / 12 - 15, marginTop: 10 }} source={LOGO_IMAGE} />
+                            <View style={styles.menuButton}>
+                                <TouchableOpacity
+                                    onPress={() => this.openDrawer()}
+                                    style={{ tintColor: 'white', width: width / 9, height: width / 9, justifyContent: 'center' }}>
+                                    <Image style={{ tintColor: 'white', width: width / 9 - 10, height: width / 9 - 10 }} source={MENU_IMAGE} />
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                            <ScrollView>
+                                <View style={styles.container}>
+                                    <TextInput style={styles.inputBox}
+                                        placeholder="Wi-Fi"
+                                        placeholderTextColor="#ffffff"
+                                        selectionColor="#1a3f95"
+                                        keyboardType="email-address"
+                                        onChangeText={(wifi) => this.setState({ wifi: wifi })}
+                                        value={this.state.wifi}
+                                    />
+                                    <TextInput style={styles.inputBox}
+                                        placeholder="Password"
+                                        placeholderTextColor="#ffffff"
+                                        selectionColor="#1a3f95"
+                                        onChangeText={(pw) => this.setState({ pw: pw })}
+                                        value={this.state.pw}
+                                    />
+                                    <TouchableOpacity style={styles.buttonBox} onPress={() => this.sendWiFiSettings()}>
+                                        <Text style={styles.buttonText}>OK</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </ScrollView>
+                        </TouchableWithoutFeedback>
+                        <Footer />
+                    </Drawer>
                 </SafeAreaView>
             </KeyboardAvoidingView>
         )
     }
 }
 
+const drawerStyles = {
+    drawer: {
+        flex: 1.0,
+        backgroundColor: '#3B5998',
+    },
+    main: {
+        flex: 1.0,
+        backgroundColor: 'white'
+    }
+}
 
 const styles = StyleSheet.create({
     keyboardAvoidingContainer: {
         flex: 1
     },
+    root: {
+        flex: 1
+    },
     container: {
-        backgroundColor: '#1a3f95',
-        flexGrow: 1,
+        backgroundColor: 'white',
+        flex: 1,
         alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: height / 7
     },
     logoImage: {
         width: height / 6,
@@ -103,26 +280,45 @@ const styles = StyleSheet.create({
     inputBox: {
         width: width * 4 / 5,
         height: height / 15,
-        backgroundColor: 'rgba(255,255,255,0.3)',
+        backgroundColor: "#1a3f95",
         borderRadius: 25,
         paddingHorizontal: 16,
         fontSize: width / 25,
-        color: '#ffffff',
-        marginVertical: 5
+        color: "white",
+        marginVertical: width / 15
     },
     buttonBox: {
         width: width * 4 / 5,
         height: height / 15,
         backgroundColor: '#a8a9ad',
         borderRadius: 25,
-        marginVertical: 10,
+        marginVertical: width / 15,
         paddingVertical: 5,
         justifyContent: 'center'
     },
     buttonText: {
         fontSize: width / 25,
         fontWeight: '500',
-        color: "#ffffff",
+        color: '#1a3f95',
         textAlign: 'center'
-    }
+    },
+    headerContainer: {
+        height: height / 12,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: '#3B5998',
+    },
+    menuButton: {
+        marginLeft: 8,
+        marginRight: 8,
+        alignSelf: 'center',
+        tintColor: 'white'
+    },
+    menuContainer: {
+        flex: 1.0,
+        backgroundColor: '#3B5998',
+    },
+    drawerContainer: {
+        flex: 1,
+    },
 });
